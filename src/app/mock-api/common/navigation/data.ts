5abae1ccf6678fe933a9ece9919e949cd606519a
leminhkhoi12062000 <80814228+leminhkhoi12062000@users.noreply.github.com>
2023-10-24 23:21:20 +0700

    FE project

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,123 @@
+/* eslint-disable */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboard',
+        title: 'Dashboard',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboards'
+    },
+    {
+        id: 'configurations',
+        title: 'Configurations',
+        type: 'basic',
+        icon: 'heroicons_outline:wrench',
+        link: '/configurations'
+    },
+    {
+        id: 'classes',
+        title: 'Classes',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-bar-square',
+        link: '/classes'
+    },
+    {
+        id: 'students',
+        title: 'Students',
+        type: 'basic',
+        icon: 'heroicons_outline:users',
+        link: '/students'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboard',
+        title: 'Dashboard',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboards'
+    },
+    {
+        id: 'configurations',
+        title: 'Configurations',
+        type: 'basic',
+        icon: 'heroicons_outline:wrench',
+        link: '/configurations'
+    },
+    {
+        id: 'classes',
+        title: 'Classes',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-bar-square',
+        link: '/classes'
+    },
+    {
+        id: 'students',
+        title: 'Students',
+        type: 'basic',
+        icon: 'heroicons_outline:users',
+        link: '/students'
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboard',
+        title: 'Dashboard',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboards'
+    },
+    {
+        id: 'configurations',
+        title: 'Configurations',
+        type: 'basic',
+        icon: 'heroicons_outline:wrench',
+        link: '/configurations'
+    },
+    {
+        id: 'classes',
+        title: 'Classes',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-bar-square',
+        link: '/classes'
+    },
+    {
+        id: 'students',
+        title: 'Students',
+        type: 'basic',
+        icon: 'heroicons_outline:users',
+        link: '/students'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboard',
+        title: 'Dashboard',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboards'
+    },
+    {
+        id: 'configurations',
+        title: 'Configurations',
+        type: 'basic',
+        icon: 'heroicons_outline:wrench',
+        link: '/configurations'
+    },
+    {
+        id: 'classes',
+        title: 'Classes',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-bar-square',
+        link: '/classes'
+    },
+    {
+        id: 'students',
+        title: 'Students',
+        type: 'basic',
+        icon: 'heroicons_outline:users',
+        link: '/students'
+    }
+];
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    // baseUrl: 'https://localhost:7222',
+    baseUrl: 'https://plantdetectionservice.azurewebsites.net',
+};
